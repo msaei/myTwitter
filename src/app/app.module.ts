3555a8f38f725980207d471cda1654d9e3cc8d39
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2017-07-24 15:11:43 -0400

    angular material added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routs';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
@@ -15,6 +18,8 @@ import { UserService } from './user.service';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     routing
   ],
   providers: [
